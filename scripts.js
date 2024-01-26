e541feca9657ba26bb76fb1b0fd2dc956da4c720
giovane-f16 <giovane_16silva@hotmail.com>
2024-01-26 13:47:33 -0300

    Ajuste nas animações do menu

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 jQuery(function(){
     jQuery(".arrow-down").on("click", function() {
-        jQuery(this).closest("li").find(".features-itens").toggle();
+        jQuery(this).closest("li").find(".features-itens").toggleClass("active");
         jQuery(this).toggleClass("arrow-up");
     });
 
