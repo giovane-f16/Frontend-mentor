9e627beaaad53d437479abe791031a64b322d79b
giovane-f16 <giovane_16silva@hotmail.com>
2024-01-27 13:47:24 -0300

    Incluindo menu mobile, não finalizado

--- scripts.js
+++ scripts.js
@@ -3,5 +3,9 @@ jQuery(function(){
         jQuery(this).closest("li").find(".features-itens").toggleClass("active");
         jQuery(this).toggleClass("arrow-up");
     });
-
+    
+    jQuery(".img-menu-mobile, .close-menu-mobile").on("click", function() {
+        jQuery(".menu-layer").toggle();
+        jQuery(".menu-mobile").toggleClass("active-mobile");
+    });
 });
