a1e16faf2392600ddf973fd497d9c7d0b8411776
giovane-f16 <giovane_16silva@hotmail.com>
2024-01-28 00:28:37 -0300

    Ajustes do menu mobile

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-jQuery(function(){
-    jQuery(".arrow-down").on("click", function() {
-        jQuery(this).closest("li").find(".features-itens").toggleClass("active");
-        jQuery(this).closest("li").find(".features-itens-mobile").toggleClass("features-itens-mobile-active");
-        jQuery(this).toggleClass("arrow-up");
-    });
+jQuery(function() {
+    // jQuery(".arrow-down").on("click", function() {
+    //     jQuery(this).closest("li").find(".features-itens").toggleClass("active");
+    //     jQuery(this).closest("li").find(".features-itens-mobile").toggleClass("features-itens-mobile-active");
+    //     jQuery(this).toggleClass("arrow-up");
+    // });
     
-    jQuery(".img-menu-mobile, .close-menu-mobile").on("click", function() {
-        jQuery(".menu-layer").toggle();
+    jQuery(".img-menu-mobile, .close-menu-mobile, .menu-layer").on("click", function() {
+        jQuery(".menu-layer").toggle(10);
         jQuery(".menu-mobile").toggleClass("active-mobile");
+    });
+
+    jQuery(".feature").on("click", function () {
+        jQuery(this).find(".features-itens-mobile").toggleClass("features-itens-mobile-active");
+        jQuery(this).find(".arrow-down").toggleClass("arrow-up");
     });
 });
