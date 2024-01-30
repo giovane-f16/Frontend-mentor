b0d5b2c1db003abc10210bf64efd53d7694028cf
giovane-f16 <giovane_16silva@hotmail.com>
2024-01-29 21:34:15 -0300

    Ajustes finais

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-jQuery(function() {
-    // jQuery(".arrow-down").on("click", function() {
-    //     jQuery(this).closest("li").find(".features-itens").toggleClass("active");
-    //     jQuery(this).closest("li").find(".features-itens-mobile").toggleClass("features-itens-mobile-active");
-    //     jQuery(this).toggleClass("arrow-up");
-    // });
-    
+jQuery(function() {    
+    jQuery(".menu li").on("click", function() {
+        jQuery(this).find(".features-itens").toggleClass("active");
+        jQuery(this).find(".arrow-down").toggleClass("arrow-up");
+    });
+
     jQuery(".img-menu-mobile, .close-menu-mobile, .menu-layer").on("click", function() {
-        jQuery(".menu-layer").toggle(10);
+        jQuery(".menu-layer").toggle();
         jQuery(".menu-mobile").toggleClass("active-mobile");
     });
 
     jQuery(".feature").on("click", function () {
         jQuery(this).find(".features-itens-mobile").toggleClass("features-itens-mobile-active");
         jQuery(this).find(".arrow-down").toggleClass("arrow-up");
+    });
+
+    jQuery("main div a").on("click", function (e) {
+       e.preventDefault();
     });
 });
